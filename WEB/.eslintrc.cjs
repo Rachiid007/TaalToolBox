f6af64f485ea3e7495cc37b200b81f965c3af5c1
Rachiid007 <abderrachid.bellaali@gmail.com>
2022-09-17 14:30:32 +0200

    🏗️ Project scaffolding

---
+++ WEB/.eslintrc.cjs
@@ -0,0 +1,10 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: ['plugin:vue/vue3-essential', '@vue/prettier', 'eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
---
+++ WEB/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Projet Langue</h1>
+    <p>Projet d'intégration App Web et Mobile.</p>
+  </main>
+</template>
