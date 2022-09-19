568f67b6178a81bb8368816db58a63dae74976c1
CardinPatson <cardin.tiako@gmail.com>
2022-09-19 20:56:15 +0200

    ➕🔧 add talwind css and configure her file

--- WEB/src/App.vue
+++ WEB/src/App.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- Hello word component -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p class="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
+      <p class="text-gray-500 text-lg">Vue 3 and Tailwind CSS ready</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+import { defineComponent } from 'vue'
+
+// export le composant principale et les composants qu'ils utilisent
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
 </script>
 
-<template>
-  <RouterView />
-</template>
+<style></style>
