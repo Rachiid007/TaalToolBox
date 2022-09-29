2a15e2a7a79733c83dd8cd0736d5fe840ca34bd9
Patson <cardin.tiako@gmail.com>
2022-09-29 12:35:29 +0200

    🔨refactor(map) : refactor to vue 3

--- WEB/src/components/Map.vue
+++ WEB/src/components/Map.vue
@@ -2,18 +2,49 @@
   <div id="map"></div>
 </template>
 
-<script>
+<script setup>
 import { Point } from 'ol/geom'
 import { fromLonLat } from 'ol/proj'
 import { Map, View, Feature } from 'ol'
 import { Tile as TileLayer, Vector as VectorLayer } from 'ol/layer'
-import { OSM,Vector as VectorSource } from 'ol/source'
+import { OSM, Vector as VectorSource } from 'ol/source'
+import { Icon, Style } from 'ol/style'
+import { Modify } from 'ol/interaction'
 import MapView from '../views/MapView.vue'
+import { onMounted, ref } from 'vue'
+import useMapStore from '../stores/map'
 
-export default {
-  name: 'Map',
-  mounted() {
-    this.map = new Map({
+const mapStore = useMapStore()
+
+const iconStyle = new Style({
+  image: new Icon({
+    anchor: [0.5, 46],
+    anchorXUnits: 'fraction',
+    anchorYUnits: 'pixels',
+    src: 'src/assets/images/geo-alt-isj.svg',
+  })
+})
+
+const ISJFeature = new Feature({
+  geometry: new Point(fromLonLat([4.39064, 50.83756])), //[4.39064, 50.83756],
+  name: 'Institut Saint Joseph'
+})
+
+const IDBFeature = new Feature({
+  geometry: new Point(fromLonLat([4.42537, 50.83826])) //[4.39064, 50.83756]
+})
+
+const ICMFeature = new Feature({
+  geometry: new Point(fromLonLat([4.37576, 50.87358])) //[4.39064, 50.83756]
+})
+ISJFeature.setStyle(iconStyle)
+IDBFeature.setStyle(iconStyle)
+ICMFeature.setStyle(iconStyle)
+
+onMounted(() => {
+  // mapStore.setMap(
+  const map2 = ref(
+    new Map({
       target: 'map',
       layers: [
         new TileLayer({
@@ -22,11 +53,17 @@ export default {
         }),
         new VectorLayer({
           source: new VectorSource({
-            features: [
-              new Feature({
-                geometry: new Point(fromLonLat([4.39064, 50.83756])) //[4.39064, 50.83756]
-              })
-            ]
+            features: [ISJFeature]
+          })
+        }),
+        new VectorLayer({
+          source: new VectorSource({
+            features: [IDBFeature]
+          })
+        }),
+        new VectorLayer({
+          source: new VectorSource({
+            features: [ICMFeature]
           })
         })
       ],
@@ -36,18 +73,9 @@ export default {
         zoom: 12
       })
     })
-    // var layer = new VectorL({
-    //   source: new VectorS({
-    //     features: [
-    //       new Feature({
-    //         geometry: new Point(fromLonLat([4.39064, 50.83756])) //[4.39064, 50.83756]
-    //       })
-    //     ]
-    //   })
-    // })
-    // this.map.addLayer(layer)
-
-    // this.$store.commit('setMap', this.map)
-  }
-}
+  )
+  console.log(map2)
+})
+// this.$store.commit('setMap', this.map)
+// }:
 </script>
