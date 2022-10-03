eb99e204acdc9ff669ddb3328e23ba4d536bdb7f
HaAymar <he201905@students.ephec.be>
2022-10-03 09:02:35 +0200

    [UPDATE] Update the previous commit

--- WEB/src/router/index.ts
+++ WEB/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
 import MapView from '../views/MapView.vue'
 import CardView  from '../views/CardView.vue'
+import SelfAssessment from '../views/SelfAssessment.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -14,7 +15,12 @@ const router = createRouter({
       path: '/cards',
       name: 'cards',
       component: CardView
-    }
+    },
+    {
+      path: '/selfAss',
+      name: 'selfAssessment',
+      component: SelfAssessment
+    },
   ]
 })
 
---
+++ WEB/src/views/SelfAssessment.vue
@@ -0,0 +1,57 @@
+<script lang="ts">
+
+</script>
+
+<template>
+    <!-- <Card /> -->
+    <div class="container">
+        <input id="getAnswer" type="button" value="J'ai trouvé!">
+        <input id="almost" type="button" value="J'etais presque !">
+        <input id="noAnswer" type="button" value="J'ai pas trouvé!">  
+    </div>
+        
+     
+ </template>
+
+<style>
+   
+    #getAnswer{
+        margin: 30px;
+        background-color: green;
+        border-radius: 10px;
+        font-size: 15px;
+        width: 150px;
+        height: 60px;
+        color: white;
+        padding: 20px;
+        text-align: center;
+        cursor: pointer;
+    }
+
+    #almost{
+        margin: 30px;
+        background-color: rgb(196, 196, 60);
+        border-radius: 10px;
+        font-size: 15px;
+        width: 150px;
+        height: 60px;
+        color: white;
+        padding: 20px;
+        text-align: center;
+        cursor: pointer;
+    }
+
+    #noAnswer{
+        margin: 30px;
+        background-color: red;
+        border-radius: 10px;
+        font-size: 15px;
+        width: 150px;
+        height: 60px;
+        color: white;
+        padding: 20px;
+        text-align: center;
+        cursor: pointer;
+    }
+    </style>
+    
