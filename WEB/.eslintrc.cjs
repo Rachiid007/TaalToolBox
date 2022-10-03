8f3f427cb1a027d9c2f80c21eb3266765b22c1b3
Rachiid007 <abderrachid.bellaali@gmail.com>
2022-10-03 14:20:55 +0200

    🐛 fix(Activity view): Fixed Activity View not showing all components

--- WEB/.eslintrc.cjs
+++ WEB/.eslintrc.cjs
@@ -11,5 +11,9 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    // 'prettier/prettier': 'error',
+    useTabs: false
   }
 }
---
+++ WEB/src/components/card/ButtonValidation.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+  import { useCardStore } from '@/stores/card'
+  const store = useCardStore()
+
+  const decreaseRemaining = () => {
+    store.decrement()
+  }
+
+  const getAnswer = () => {
+    console.log('getAnswer')
+    decreaseRemaining()
+  }
+
+  const almost = () => {
+    console.log('almost')
+    decreaseRemaining()
+  }
+
+  const noAnswer = () => {
+    console.log('noAnswer')
+    decreaseRemaining()
+  }
+</script>
+
+<template>
+  <!-- <Card /> -->
+  <div class="container">
+    <button
+      class="btn"
+      id="getAnswer"
+      @click="getAnswer"
+    >
+      J'ai trouvé
+    </button>
+    <button
+      id="almost"
+      class="btn"
+      @click="almost"
+    >
+      J'y étais presque
+    </button>
+    <button
+      id="noAnswer"
+      class="btn"
+      @click="noAnswer"
+    >
+      Je ne savais pas
+    </button>
+  </div>
+</template>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: space-around;
+    margin: 0 auto;
+    width: 100%;
+    height: 100%;
+  }
+  .btn {
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 5px;
+  }
+  #getAnswer {
+    background-color: #4caf50;
+  }
+  #almost {
+    background-color: #ff9800;
+  }
+  #noAnswer {
+    background-color: #f44336;
+  }
+</style>
