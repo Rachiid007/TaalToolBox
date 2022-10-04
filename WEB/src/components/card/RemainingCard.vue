6dfc576789dd23123d025dec4e2f51e6451546ea
Patson <cardin.tiako@gmail.com>
2022-10-04 14:58:45 +0200

    fix(view) : limit card

--- WEB/src/components/card/RemainingCard.vue
+++ WEB/src/components/card/RemainingCard.vue
@@ -1,30 +1,29 @@
 <script setup lang="ts">
-  import { useCardStore } from '@/stores/card'
-  import { computed } from 'vue'
-  const store = useCardStore()
+import { useCardStore } from '@/stores/card'
+import { computed } from 'vue'
+const store = useCardStore()
 
-  const remaining = computed(() => store.remaining)
+const remaining = computed(() => store.remaining)
 
-  const monTab = computed(() => {
-    let myArray = []
-    if (remaining.value <= 8) {
-      for (let i = 0; i < remaining.value; i++) {
-        myArray.push({
-          card: i,
-          offsetLeft: i * 7
-        })
-      }
-    } else {
-      for (let i = 0; i < 8; i++) {
-        myArray.push({
-          card: i,
-          offsetLeft: i * 7
-        })
-      }
+const monTab = computed(() => {
+  let myArray = []
+  if (remaining.value <= 8) {
+    for (let i = 0; i < remaining.value; i++) {
+      myArray.push({
+        card: i,
+        offsetLeft: i * 7
+      })
     }
-
-    return myArray
-  })
+  } else {
+    for (let i = 0; i < 8; i++) {
+      myArray.push({
+        card: i,
+        offsetLeft: i * 7
+      })
+    }
+  }
+  return myArray
+})
 </script>
 
 <template>
@@ -47,69 +46,70 @@
 </template>
 
 <style scoped>
+.remaining-container {
+  /* border: solid black 1px; */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 250px;
+  height: 600px;
+}
+.contain {
+  /* border: solid red 1px; */
+  position: relative;
+  display: flex;
+  width: 250px;
+  height: 300px;
+  justify-content: center;
+  align-items: center;
+}
+.title {
+  /* border: solid red 1px; */
+  /* margin: 15px 0; */
+  padding-left: 10px;
+  font-size: 25px;
+  color: rgba(0, 0, 0, 0.5);
+}
+.card {
+  height: 230px;
+  width: 190px;
+  border-radius: 15px;
+  border: 1px solid black;
+  background-color: white;
+  position: absolute;
+  /* top: 0; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 50px;
+  font-weight: bold;
+  color: green;
+}
+
+.card:last-of-type {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 50px;
+  font-weight: bold;
+  color: green;
+}
+@media (max-width: 768px) {
   .remaining-container {
-    /* border: solid black 1px; */
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 250px;
-    height: 600px;
+    border: solid black 1px;
+    position: relative;
+    height: 300px;
+    width: 150px;
   }
   .contain {
-    /* border: solid red 1px; */
-    position: relative;
-    display: flex;
-    width: 250px;
-    height: 300px;
-    justify-content: center;
-    align-items: center;
-  }
-  .title {
-    /* border: solid red 1px; */
-    /* margin: 15px 0; */
-    padding-left: 10px;
-    font-size: 25px;
-    color: rgba(0, 0, 0, 0.5);
+    width: 150px;
+    /* border: solid green 1px; */
   }
   .card {
-    height: 230px;
-    width: 190px;
-    border-radius: 15px;
-    border: 1px solid black;
-    background-color: white;
-    position: absolute;
-    /* top: 0; */
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 50px;
-    font-weight: bold;
-    color: green;
+    width: 100px;
+    height: 150px;
   }
-
-  .card:last-of-type {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 50px;
-    font-weight: bold;
-    color: green;
-  }
-  @media (max-width: 768px) {
-    .remaining-container {
-      /* border: solid black 1px; */
-      position: relative;
-      height: 300px;
-      width: 150px;
-    }
-    .contain {
-      width: 150px;
-      /* border: solid green 1px; */
-    }
-    .card {
-      width: 100px;
-      height: 150px;
-    }
-  }
+}
 </style>
