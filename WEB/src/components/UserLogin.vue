64cfa17da56ddcb9666745299a195585aabe880e
HaAymar <he201905@students.ephec.be>
2022-10-06 09:56:38 +0200

    :sparkles: feat(userLogin): adding a userLogin

---
+++ WEB/src/components/UserLogin.vue
@@ -0,0 +1,5 @@
+<script lang="ts">
+    export default {
+      name: 'UserLogin'
+    }
+</script>
--- WEB/src/router/index.ts
+++ WEB/src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 // import HomeView from '../views/HomeView.vue'
 import MapView from '../views/MapView.vue'
 import CardView  from '../views/CardView.vue'
+import UserLogin from '../views/UserLogin.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -14,6 +16,11 @@ const router = createRouter({
       path: '/cards',
       name: 'cards',
       component: CardView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: UserLogin
     }
   ]
 })
