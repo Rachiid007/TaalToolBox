9dd3a95416c4f352989afd81bf18e2423980af0c
Pourbaix Michaël <mi.pourbaix@students.ephec.be>
2022-10-09 13:47:27 +0200

    💄🍱✨ feat(userLogin): Improving/Reviewing css and finishing forms for the registration

--- WEB/src/components/Login.vue
+++ WEB/src/components/Login.vue
@@ -1,16 +1,18 @@
-<script lang="ts"></script>
+<script lang="ts">
+export default {}
+</script>
 
 <template>
   <div class="main">
     <div class="content">
       <div class="title-image">
-        <img src="../assets/logo/logoProjet.png" class="image" />
+        <img src="../assets/logo/logo.svg" class="image" />
         <p class="logoName">TaalToolBox</p>
       </div>
       <div class="fields2">
         <form class="form">
           <div class="fields-connection">
-            <input class="mailPass" type="text" placeholder="Mail"/>
+            <input class="mailPass" type="text" placeholder="Mail" />
             <input class="mailPass" type="text" placeholder="Mot de passe" />
           </div>
         </form>
@@ -34,41 +36,6 @@
   /* border: solid grey 1px; */
 }
 
-.title {
-  display: flex;
-  flex-direction: row;
-}
-
-.activity_name {
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-  color: grey;
-  font-weight: 400;
-  font-size: 1.8em;
-}
-
-.globalBorder {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-.borderTop {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
-.connectInscri {
-  border-bottom: solid 1px grey;
-  padding: 10px;
-  padding-left: 100px;
-  padding-right: 100px;
-  font-size: 1.5em;
-}
-
-#connection2 {
-  background-color: #026b30;
-  color: white;
-}
-
 .content {
   display: flex;
   flex-direction: column;
@@ -76,10 +43,10 @@
 }
 
 .fields-connection {
-  gap:  40px;
+  gap: 40px;
   display: flex;
   flex-direction: column;
- }
+}
 
 .buttonConnect {
   padding-bottom: 15%;
@@ -116,39 +83,37 @@ input {
   padding: 3px;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 2px 4px -2px;
   outline: none;
-} 
+}
 
- input[type="text"].mailPass {
+input[type='text'].mailPass {
   padding: 4px;
   margin-right: 100px;
   /* margin-left: 50px; */
-} 
+}
 
 input::placeholder {
-  color:#026b30;
-}
-.generalBorder {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  color: #026b30;
 }
 
 .title-image {
-  padding: 20px;
+  padding-top: 5%;
   display: flex;
   flex-direction: row;
+  justify-content: center;
   align-items: center;
-  gap: 5%;
+  gap: 2%;
+  width: 100%;
+}
+
+.image {
+  width: 10%;
+  height: auto;
 }
 
 .form {
   display: flex;
   flex-direction: column;
   gap: 100px;
-}
-
-.image {
-  width: 50px;
 }
 
 .logoName {
