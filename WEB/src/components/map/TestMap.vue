a0cdd6e4e935a2fc9c3341cfd895dad51fe63a43
Pourbaix Michaël <mi.pourbaix@students.ephec.be>
2022-10-12 20:36:58 +0200

    :sparkles: feat(new-map-test): Adding a new map with new functionalities thanks to aymar davy

--- WEB/src/components/map/TestMap.vue
+++ WEB/src/components/map/TestMap.vue
@@ -1,32 +1,120 @@
 <script setup lang="ts">
-  import Map from 'ol/Map'
   import Overlay from 'ol/Overlay'
   import TileLayer from 'ol/layer/Tile'
-  import View from 'ol/View'
   import XYZ from 'ol/source/XYZ'
   import { toLonLat } from 'ol/proj'
   import { toStringHDMS } from 'ol/coordinate'
   import { OSM } from 'ol/source'
-  import { onMounted, ref } from 'vue'
+  import { onMounted, ref, reactive } from 'vue'
+  import { Icon, Style } from 'ol/style'
+  import { Point } from 'ol/geom'
+  import { fromLonLat } from 'ol/proj'
+  import { Map, View, Feature } from 'ol'
+  import { Vector as VectorLayer } from 'ol/layer'
+  import { Vector as VectorSource } from 'ol/source'
+  import { Control, defaults as defaultControls } from 'ol/control'
+  import { defaults as defaultInteractions } from 'ol/interaction.js'
+  import { getArea } from 'ol/extent'
+
+  //
+  //
+  // TODO: Essayer de régler ce typeScript de m*rde
+  //
+  //
+
+  // Cette ref correspond à la div du popup
+  const popup = ref(null)
+  // On utilise cet état pour afficher ou désafficher le popup
+  const popupVisibility = ref(false)
+  // Cette ref permet de stocker le nom du niveau à ensuite afficher
+  const levelName = ref('')
+  // Cette ref permet de stocker le numéro du niveau à ensuite afficher
+  const levelNumber = ref(1)
+  // Ici on stocke les différents points que l'on va afficher sur la map
+  const pointState = reactive({
+    points: [
+      {
+        label: 'Institut Saint Joseph',
+        coordinates: [4.39064, 50.83756],
+        levelId: 1
+      },
+      {
+        coordinates: [4.42537, 50.83826], //[4.39064, 50.83756]
+        label: 'Institut Don Bosco',
+        levelId: 2
+      },
+      {
+        coordinates: [4.37576, 50.87358], //[4.39064, 50.83756]
+        label: 'Institut Cardinal Mercier',
+        levelId: 3
+      }
+    ]
+  })
+  // Permet de désactiver le dézoom et les mouvements sur la carte quand le popup est ouvert
+  const allowControls = ref(true)
 
   const onCloserClick = () => {
-    document.getElementById('popup').style.display = 'none'
+    popupVisibility.value = false
     // closer.style.display = 'none'
     return false
   }
-  const closer = document.getElementById('popup-closer')
-  onMounted(() => {
-    const container = document.getElementById('popup')
-    const content = document.getElementById('popup-content')
 
+  onMounted(() => {
+    function setActive() {
+      for (let intercation in map.getInteractions().getArray()) {
+        let template = map.getInteractions().getArray()
+        template[intercation].setActive(true)
+      }
+    }
+
+    function setNotActive() {
+      for (let intercation in map.getInteractions().getArray()) {
+        let template = map.getInteractions().getArray()
+        template[intercation].setActive(false)
+      }
+    }
+
+    const iconStyle = new Style({
+      // stroke: new Stroke({
+      //       width: 5,
+      //       color: "#ff0000"
+      //     },
+      image: new Icon({
+        anchor: [10, 10],
+        anchorXUnits: 'pixels',
+        anchorYUnits: 'pixels',
+        src: 'src/assets/images/geo-marker.svg',
+        displacement: [-5, 25],
+        scale: 1
+      })
+    })
     const overlay = new Overlay({
-      element: container,
+      element: popup.value,
       autoPan: {
         animation: {
           duration: 250
         }
       }
     })
+    // const ISJFeature = new Feature({
+    //   geometry: new Point(fromLonLat([4.39064, 50.83756])), //[4.39064, 50.83756],
+    //   name: 'Institut Saint Joseph',
+    //   id: 1
+    // })
+    // const IDBFeature = new Feature({
+    //   geometry: new Point(fromLonLat([4.42537, 50.83826])), //[4.39064, 50.83756]
+    //   name: 'Institut Don Bosco',
+    //   id: 2
+    // })
+    // const ICMFeature = new Feature({
+    //   geometry: new Point(fromLonLat([4.37576, 50.87358])), //[4.39064, 50.83756]
+    //   name: 'Institut Cardinal Mercier',
+    //   id: 3
+    // })
+
+    // ISJFeature.setStyle(iconStyle)
+    // IDBFeature.setStyle(iconStyle)
+    // ICMFeature.setStyle(iconStyle)
 
     const map = new Map({
       layers: [
@@ -37,16 +125,61 @@
       overlays: [overlay],
       target: 'map',
       view: new View({
-        center: [485151.97, 6586152.84],
+        center: [489191.97, 6595852.84],
         // center: fromLonLat([4.39064, 50.83756]),
-        zoom: 12
+        zoom: 12,
+        minZoom: 11
+      }),
+      controls: defaultControls({
+        attribution: false,
+        zoom: false,
+        rotate: false
+      }),
+      interactions: defaultInteractions({
+        // doubleClickZoom: allowControls.value,
+        // dragPan: allowControls.value,
+        // keyboardZoom: allowControls.value,
+        // mouseWheelZoom: allowControls.value
       })
     })
 
+    // ---------------------------------------------------------------
+    // On génère les points sur la map à partir de la liste des objets
+    // ---------------------------------------------------------------
+
+    for (let item in pointState.points) {
+      let point = pointState.points[item]
+      let feature = new Feature({
+        geometry: new Point(fromLonLat([point.coordinates[0], point.coordinates[1]])), //[4.39064, 50.83756]
+        name: point.label
+      })
+      feature.setId(point.levelId)
+      feature.setStyle(iconStyle)
+      let vector = new VectorLayer({
+        source: new VectorSource({
+          features: [feature]
+        })
+      })
+      // On ajoute la nouvelle feature à la fin de la liste des layers de la map
+      map.getLayers().extend([vector])
+    }
+
     map.on('singleclick', function (evt) {
-      const coordinate = evt.coordinate
-      document.getElementById('popup').style.display = 'block'
-      overlay.setPosition(coordinate)
+      // Si on click autre part que sur un point, le popup se désaffiche
+      popupVisibility.value = false
+      setActive()
+      console.log(map.getInteractions().getArray())
+      map.forEachFeatureAtPixel(evt.pixel, function (feature, layer: any) {
+        // On attribue les valeurs de nom de niveau et de numéro de niveau
+        levelName.value = feature.getProperties().name
+        levelNumber.value = feature.getId()
+        // On enregistre les coordonnées pour mettre le popup au bon endroit
+        const coordinate = evt.coordinate
+        overlay.setPosition(coordinate)
+        popupVisibility.value = true
+        allowControls.value = false
+        setNotActive()
+      })
     })
   })
 </script>
@@ -57,6 +190,8 @@
     class="map"
   ></div>
   <div
+    v-show="popupVisibility"
+    ref="popup"
     id="popup"
     class="ol-popup"
   >
@@ -70,8 +205,8 @@
       id="popup-content"
       class="popup-content"
     >
-      <p class="popup-title">Level 1</p>
-      <p class="level-details">Quizz Flashcard 1</p>
+      <p class="popup-title">{{ levelName }}</p>
+      <p class="level-details">Quizz Flashcard {{ levelNumber }}</p>
       <img
         class="gamemode-image"
         src="@/assets/logo/flashcards.svg"
