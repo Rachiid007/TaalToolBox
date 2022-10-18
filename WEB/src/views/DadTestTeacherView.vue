d40007882eb1210b68009b5172cd1a6f92a9a282
Pourbaix Michaël <mi.pourbaix@students.ephec.be>
2022-10-18 12:24:04 +0200

    :sparkles: feat(drag-and-drop): Beginning the creation of the drag-and-drop page for the teacher and fixing a missed written sentence in the home page

---
+++ WEB/src/views/DadTestTeacherView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  import DragndropTeacher from '@/components/testDragNDrop/dragndropteacher.vue'
+  import TheHeaderDnd from '@/components/TheHeaderDragNDrop.vue'
+</script>
+<template>
+  <div class="page">
+    <TheHeaderDnd />
+    <DragndropTeacher />
+  </div>
+</template>
+<style scoped>
+  .page {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 30px;
+    overflow-x: hidden;
+  }
+</style>
