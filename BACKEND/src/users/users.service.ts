ee1757aad230900da80a2b61030f40b7b0bde966
bricekouetcheu <brcronld@gmail.com>
2022-10-18 22:17:35 +0200

    login and registration set up fixed

--- BACKEND/src/users/users.service.ts
+++ BACKEND/src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 import { Users } from './entities/users.entity';
 
 @Injectable()
@@ -14,7 +15,8 @@ export class UsersService {
     return await this.userRepository.findOne(data);
   }
 
-  async create(data) {
+  async create(data: CreateUserDto): Promise<any> {
+    console.log(data);
     return await this.userRepository
       .save(data)
       .then((res) => res)
