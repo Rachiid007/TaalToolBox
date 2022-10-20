171d856b597ba7b0032f7d6ad8ea3ae8f13948c0
Louis Lombaert <l.lombaert@students.ephec.be>
2022-10-20 16:55:54 +0200

    chgmt css + simplification

---
+++ WEB/src/components/profile/Class.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="class">
+        Exemple classe 
+    </div>
+</template>
+
+<script  setup lang="ts">
+
+</script>
+<style>
+    .class{
+        text-align: center;
+    }
+</style>
--- WEB/src/views/ProfileView.vue
+++ WEB/src/views/ProfileView.vue
@@ -1,30 +1,17 @@
 <script setup lang="ts">
-  import Profile from '@/components/Profile.vue'
-  import Class from '@/components/Class.vue'
-  import Success from '@/components/Success.vue'
+  import Profile from '@/components/profile/Profile.vue'
+  import Class from '@/components/profile/Class.vue'
+  import Success from '@/components/profile/Success.vue'
   import MainHeader from '@/components/headers/MainHeader.vue'
   import { ref } from 'vue'
   //const store = useCardStore()
   //const remaining = computed(() => store.remaining)
-  const compProfile = ref(true)
-  const compClass = ref(true)
-  const compSuccess = ref(true)
+  const comp = ref(0)
 
-  function showProfile(){
-    compProfile.value = true
-    compClass.value = false
-    compSuccess.value = false
+  function changeComp(nb: number){
+    comp.value = nb;
   }
-  function showClass(){
-    compProfile.value = false
-    compClass.value = true
-    compSuccess.value = false
-  }
-  function showSuccess(){
-    compProfile.value = false
-    compClass.value = false
-    compSuccess.value = true
-  }
+
   function logOut(){
     //à completer
   }
@@ -36,27 +23,27 @@
   </div>
   <div class="contents">
     <div class="buttons">
-      <button @click="showProfile">
+      <button @click="changeComp(0)">
         Profil
       </button>
-      <button @click="showClass">
+      <button @click="changeComp(1)">
         Classe
       </button>
-      <button @click="showSuccess">
-        Succés
+      <button @click="changeComp(2)">
+        Succès
       </button>
       <button @click="logOut">
         Log out
       </button>
     </div>
     <div class="compP">
-      <div v-if="compProfile">
+      <div v-if="comp == 0">
         <Profile />
       </div>
-      <div v-else-if="compClass">
+      <div v-else-if="comp == 1">
         <Class />
       </div>
-      <div v-else="compSuccess">
+      <div v-else="comp == 2">
         <Success />
       </div>
     </div>
@@ -64,7 +51,10 @@
 </template>
 
 <style scoped>
-
+  .contents{
+    display: flex;
+    flex-direction: row;
+  }
   button {
     display: block;
     background-color: #fdfdfd;
@@ -88,5 +78,16 @@
   .buttons{
     float: left;
     margin-top: 5%;
+  }
+  .compP{
+    border: ridge 5px #cccccc;
+    height: 20em;
+    margin-top: 6%;
+    margin-left: 2%;
+    padding: 5%;
+    width: 80%;
+    display: block;
+    float: left;
+    font-size: 28px;
   }
 </style>
