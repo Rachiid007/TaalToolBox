04fca7441a5bf4f8bba6ebd2d3afb147e77b74ab
Pourbaix Michaël <mi.pourbaix@students.ephec.be>
2022-10-24 23:14:52 +0200

    :sparkles: :lipstick: feat(choiceActivities): Adding v-for generation for cards and restyling some shit

--- WEB/src/components/Activities/ChooseActivities.vue
+++ WEB/src/components/Activities/ChooseActivities.vue
@@ -1,133 +1,161 @@
 <script setup lang="ts">
+  import { ref, onMounted } from 'vue'
 
-import { ref, onMounted } from 'vue'
+  const selectedActivity = ref(0)
+  const isThereAnyActivitySelected = ref(false)
 
-const details = ref()
+  const listActivities = [
+    {
+      activity_name: 'FlashCards',
+      url: '/src/assets/logo/flashcards.svg',
+      description:
+        "Activité dans laquelle l'étudiant doit trouver de mots et doit s'auto-évaluer. Les mots" +
+        '            sont representé sous forme de carte. Un systeme de poids est mise en place pour que les' +
+        "            cartes plus dure de l'élève réaparaissent le plus souvent."
+    },
+    {
+      activity_name: 'DragAndLearn',
+      url: '/src/assets/logo/dalcard.svg',
+      description:
+        "Activité dans laquelle l'étudiant doit trouver de mots et doit s'auto-évaluer. Les mots" +
+        '            sont representé sous forme de carte. Un systeme de poids est mise en place pour que les' +
+        "            cartes plus dure de l'élève réaparaissent le plus souvent."
+    }
+  ]
 
-const listActivities = {
-    flashCards: "FlashCards", 
-    DragAndLearn: "DragAndLearn"}
-
+  const handleClick = (divId: any) => {
+    isThereAnyActivitySelected.value = true
+    console.log(divId)
+    selectedActivity.value = divId
+  }
 </script>
 <template>
-    <div class="main">
-        <div class="title">
-            <p class="text">Choisissez le type d'activité</p>
-            <hr class = "ligne">
-            <p class="underline"></p>
-        </div>
-        <div class="content">
-            <div class="typeActi">
-              <div class="carte">
-               <img src="../../assets/logo/dalcard.svg" class="logo">
-              </div>
-              <div class="carte">
-               <img src="../../assets/logo/flashcards.svg" class="logo">  
-              </div>
-            </div>
-            <div class="description">
-               <div class="descrTitle">Type sélectionné:</div>
-               <div class="actiName">FlashCards</div>
-               <div class="textDescrip">
-                <p>Activité dans laquelle l'étudiant doit trouver de mots et doit s'autoevaluer. Les mots
-                    sont representé sous forme de carte. Un systeme de poids est mise en place pour que les
-                    cartes plus dure de l'élève réaparaissent le plus souvent.
-                </p>
-               </div>
-               <div>
-                <button class="button">Créer une activité</button>
-               </div>
-            </div>
-        </div>
+  <div class="main">
+    <div class="title">
+      <p class="text">Choisissez le type d'activité</p>
     </div>
+    <div class="content">
+      <div class="typeActi">
+        <div
+          class="carte"
+          v-for="(value, key) in listActivities"
+          :id="key.toString()"
+          @click="handleClick(key)"
+        >
+          <img
+            :src="value.url"
+            alt="Gamemode logo"
+          />
+        </div>
+      </div>
+      <div class="description">
+        <div class="descrTitle">Type sélectionné:</div>
+        <div
+          class="actiName"
+          v-show="isThereAnyActivitySelected"
+        >
+          {{ isThereAnyActivitySelected ? listActivities[selectedActivity].activity_name : '' }}
+        </div>
+        <div
+          class="textDescrip"
+          v-show="isThereAnyActivitySelected"
+        >
+          <p>
+            {{ isThereAnyActivitySelected ? listActivities[selectedActivity].description : '' }}
+          </p>
+        </div>
+        <div>
+          <button class="button">Créer une activité</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <style scoped>
-.main{
-     gap: 20px;
-     padding:40px;
-     display: flex;
-     flex-direction: column;
-     align-items: center;
-}
- 
-.title {
-}
+  .main {
+    gap: 50px;
+    padding-top: 75px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+    max-width: 1600px;
+  }
 
-.carte{
-   display: flex;
-   align-items: center;
-   justify-content: center;
-   width: 18%;
-   height: 45%;
-   border: 2px solid grey; 
-   border-radius: 15px;
-   box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.10) 0px 8px 10px;
-   padding: 5px;
-   margin-bottom: 17px;
-    
-}
+  .title {
+    padding-top: 25px;
+  }
 
-hr.ligne{
-  /* border: 4px solid green;
-  border-radius: 5px;   */
-  border: 1px solid green;
-  background-color: green;
-}
+  .carte {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 20%;
+    height: 48%;
+    border: 2px solid grey;
+    border-radius: 15px;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.1) 0px 8px 10px;
+    padding: 5px;
+  }
 
-.text{
+  .carte:hover {
+    cursor: pointer;
+  }
+
+  .carte:active {
+    transform: scale(1.1);
+  }
+  .text {
     font-size: 1.8em;
     color: grey;
-}
-.content{
-    gap:5%;
-    /* width: 90%;
-    height: 50%; */
+    border-bottom: 2px solid green;
+    font-weight: 600;
+  }
+  .content {
+    gap: 5%;
+    width: 90%;
+    height: 70%;
     display: flex;
-    flex-direction:row;
+    flex-direction: row;
     /* align-items: center; */
     justify-content: center;
-    width: 100%;
-    height: 100vh;
-    
-}
-.typeActi{
-    gap: 3%;
-    height: 60%;
-    display: flex; 
-    flex-direction: row; 
+  }
+  .typeActi {
+    gap: 4%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
     /* align-items: flex-start; */
-    border: 1px black solid;
+    border: 2px grey solid;
     border-radius: 5px;
-   
-    width: 65%;
-     
+    width: 60%;
     padding: 20px;
-}
+  }
 
-.descrTitle{
+  .descrTitle {
     color: green;
-    font-size: 1em;
-}
+    font-size: 1.4em;
+  }
 
-.description{
-    border: solid 1px black;
+  .description {
+    border: solid 2px grey;
     /* width: 20%; */
     border-radius: 5px;
-    padding: 25px 10px;
-    padding-left: 30px;
-    padding-right: 30px;
-    width: 20%;
-    height: 60%;
+    padding: 10px 10px;
+    width: 22%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     gap: 40px;
-}
-.actiName{
-    font-size: 1.2em;
+  }
+  .actiName {
+    font-size: 1.4em;
     color: grey;
-}
-.button{
+    font-weight: bold;
+  }
+  .button {
     background-color: green;
     display: flex;
     flex-direction: column;
@@ -141,13 +169,14 @@ hr.ligne{
     margin: 4px 2px;
     cursor: pointer;
     border-radius: 5px;
-}
-.textDescrip{
+  }
+  .textDescrip {
     text-align: center;
+    font-style: italic;
     /* font-family:monospace; */
-}
-.logo{
-    width:90%;
+  }
+  .logo {
+    width: 90%;
     height: auto;
-}
+  }
 </style>
