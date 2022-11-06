8cf0eafc3ad62305803663466a7a326b91ce7949
Patson <cardin.tiako@gmail.com>
2022-11-06 14:56:11 +0100

    perf(default role) add default role in database

--- BACKEND/src/role/role.service.ts
+++ BACKEND/src/role/role.service.ts
@@ -1,9 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
-
+import { Role } from './entities/role.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 @Injectable()
 export class RoleService {
+  constructor(
+    @InjectRepository(Role)
+    private roleRepository: Repository<Role>,
+  ) {}
+  async onApplicationBootstrap() {
+    const role = await this.roleRepository.find();
+    if (!role.length) {
+      return this.roleRepository.save([
+        {
+          name: 'Administrateur',
+          description: 'Rôle Administrateur',
+        },
+        {
+          name: 'Créateur',
+          description: 'Rôle Créateur',
+        },
+        {
+          name: 'Professeur',
+          description: 'Rôle Professeur',
+        },
+        {
+          name: 'Elève',
+          description: 'Rôle Elève',
+        },
+      ]);
+      
+    }
+  }
+
   create(createRoleDto: CreateRoleDto) {
     return 'This action adds a new role';
   }
