d38f0a329491b494fd91a9a3e98e4137ed9577b2
Louis Lombaert <l.lombaert@students.ephec.be>
2022-11-13 14:45:23 +0100

    feat: create drag and drop table + routes

---
+++ BACKEND/src/drag_and_drop/drag_and_drop.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { DragService } from './drag_and_drop.service';
+import { DragController } from './drag_and_drop.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dragAndDrop } from './entities/drag_and_drop.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([dragAndDrop])],
+  controllers: [DragController],
+  providers: [DragService],
+})
+export class DragModule {}
