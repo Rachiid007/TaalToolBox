35b1cab542740e8c319bebd99564799d4aa273bc
Pourbaix Michaël <mi.pourbaix@students.ephec.be>
2022-11-14 14:03:39 +0100

    fix(Header): Rework of the main Header (rework hamburger + tabs).

--- WEB/src/components/Login.vue
+++ WEB/src/components/Login.vue
@@ -42,10 +42,13 @@
   <div class="main">
     <div class="content">
       <div class="title">
-        <p class="connect">Connection</p>
+        <p class="connect">Connexion</p>
       </div>
       <div class="title-image">
-        <img src="../assets/logo/logo.svg" class="image" />
+        <img
+          src="../assets/logo/logo.svg"
+          class="image"
+        />
         <p class="logoName">TaalToolBox</p>
       </div>
       <div class="fields2">
@@ -83,132 +86,130 @@
 </template>
 
 <style scoped>
-.main {
-  padding: 70px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
+  .main {
+    padding: 70px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 30px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border: solid grey 1px;
-  max-width: 450px;
-}
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: solid grey 1px;
+    max-width: 450px;
+  }
 
-.title-image {
-  padding-top: 5%;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  gap: 2%;
-  width: 100%;
-}
+  .title-image {
+    padding-top: 5%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 2%;
+    width: 100%;
+  }
 
-.image {
-  width: 10%;
-  height: auto;
-}
-.logoName {
-  font-family: Segoe print;
-  /* padding-right: 10px; */
-  color: #707070;
-  font-size: 1.5em;
-}
+  .image {
+    width: 10%;
+    height: auto;
+  }
+  .logoName {
+    font-family: Segoe print;
+    /* padding-right: 10px; */
+    color: #707070;
+    font-size: 1.5em;
+  }
 
-.clickButton2 {
-  border: solid 1px black;
-  margin: 5px;
-  border-radius: 5px;
-  font-size: 15px;
-  width: 100px;
-  height: 40px;
-}
-.fields-connection {
-  padding-top: 30px;
-  gap: 40px;
-  display: flex;
-  flex-direction: column;
-}
+  .clickButton2 {
+    border: solid 1px black;
+    margin: 5px;
+    border-radius: 5px;
+    font-size: 15px;
+    width: 100px;
+    height: 40px;
+  }
+  .fields-connection {
+    padding-top: 30px;
+    gap: 40px;
+    display: flex;
+    flex-direction: column;
+  }
 
-input {
-  color: #026b30;
-  
-  width: 100%;
-  border-radius: 5px;
-  padding: 3px;
-  box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 2px 4px -2px;
-  outline: none;
-  margin-right: 50px;
-}
+  input {
+    color: #026b30;
 
-/* input[type='text'].mailPass {
+    width: 100%;
+    border-radius: 5px;
+    padding: 3px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 2px 4px -2px;
+    outline: none;
+    margin-right: 50px;
+  }
+
+  /* input[type='text'].mailPass {
   padding: 4px;
   margin-right: 100px;
 } */
 
-input::placeholder {
-  color: #026b30;
-}
+  input::placeholder {
+    color: #026b30;
+  }
 
-.buttonConnect {
-  padding-bottom: 5%;
-  padding-top: 15%;
-  gap: 200px;
-  display: flex;
-  flex-direction: row;
-  align-self: center;
-}
+  .buttonConnect {
+    padding-bottom: 5%;
+    padding-top: 15%;
+    gap: 200px;
+    display: flex;
+    flex-direction: row;
+    align-self: center;
+  }
 
-.clickButton {
-  padding: 5px 5px 5px 5px;
-  border-radius: 5px;
-  font-size: 1.1em;
-  height: 40px;
-}
+  .clickButton {
+    padding: 5px 5px 5px 5px;
+    border-radius: 5px;
+    font-size: 1.1em;
+    height: 40px;
+  }
 
-.form {
-  display: flex;
-  flex-direction: column;
-  gap: 25px;
-}
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+  }
 
-.logoName {
-  font-family: Segoe print;
-  color: #707070;
-  font-size: 1.5em;
-}
-.title {
-  min-width: 400px;
-  width: 100%;
-  background-color: #026b30;
-}
-.connect{
-  display: flex;
-  justify-content: center;
-  background-color: #026b30;
-  color: white;
-  flex-grow: 5;
-  border-bottom: solid 1px grey;
-  font-size: 1.5em;
-}
-.error {
-  color: red;
-  padding-bottom: 5%;
-  font-weight: bold;
-}
-#suivant {
-  color: white;
-  background-color: #026b30;
-}
-
+  .logoName {
+    font-family: Segoe print;
+    color: #707070;
+    font-size: 1.5em;
+  }
+  .title {
+    min-width: 400px;
+    width: 100%;
+    background-color: #026b30;
+  }
+  .connect {
+    display: flex;
+    justify-content: center;
+    background-color: #026b30;
+    color: white;
+    flex-grow: 5;
+    border-bottom: solid 1px grey;
+    font-size: 1.5em;
+  }
+  .error {
+    color: red;
+    padding-bottom: 5%;
+    font-weight: bold;
+  }
+  #suivant {
+    color: white;
+    background-color: #026b30;
+  }
 </style>
-
