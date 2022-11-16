d6c3f381b9ede102851424d434cfa7f7807fbd4f
bricekouetcheu <brcronld@gmail.com>
2022-11-16 17:14:16 +0100

    get statistics service

--- BACKEND/src/statistic/statistic.controller.ts
+++ BACKEND/src/statistic/statistic.controller.ts
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-import { Controller, Body, Patch, Param, Delete, Get } from '@nestjs/common';
-=======
+
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
->>>>>>> b5c54a6 (statistic resources created)
+
 import { StatisticService } from './statistic.service';
 import { CreateStatisticDto } from './dto/create-statistic.dto';
 import { UpdateStatisticDto } from './dto/update-statistic.dto';
@@ -11,28 +9,28 @@ import { UpdateStatisticDto } from './dto/update-statistic.dto';
 export class StatisticController {
   constructor(private readonly statisticService: StatisticService) {}
 
-  @Post()
+  /* @Post()
   create(@Body() createStatisticDto: CreateStatisticDto) {
     return this.statisticService.create(createStatisticDto);
-  }
+  }*/
 
-  @Get()
+  /* @Get()
   findAll() {
     return this.statisticService.findAll();
-  }
+  }*/
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.statisticService.findOne(+id);
+  findOne(@Param('id') id: number) {
+    return this.statisticService.Get_User_stat(+id);
   }
 
-  @Patch(':id')
+  /* @Patch(':id')
   update(@Param('id') id: string, @Body() updateStatisticDto: UpdateStatisticDto) {
     return this.statisticService.update(+id, updateStatisticDto);
-  }
+  }*/
 
-  @Delete(':id')
+  /*@Delete(':id')
   remove(@Param('id') id: string) {
     return this.statisticService.remove(+id);
-  }
+  }*/
 }
