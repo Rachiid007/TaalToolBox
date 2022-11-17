1f9b0856597eb37c675e1ab696a21ea8a0eb34f2
Ikram <HE201916@students.ephec.be>
2022-11-17 01:22:56 +0100

    [ADD] Comments

--- BACKEND/src/cards/cards.service.ts
+++ BACKEND/src/cards/cards.service.ts
@@ -30,24 +30,26 @@ export class CardsService {
     return card;
   }
 
+  //Cette fonction permet de récuperer des cartes pour un nouveau jeux selon leurs poids, le calcul du poids est basé sur proficiency.weight et answer.weight
   async findMany(id_user: number, nbrcards: number) {
     const weight_aquis = process.env.weight_aquis;
     const weight_inconnu = process.env.weight_inconnu;
     const weight_apprentissage = process.env.weight_apprentissage;
 
+    //cette requête permet de recupérer nbrcards cartes pour l'utilisateur id_user selon les critères Inconnu, en appretissage ou aquis.
     const card = this.cardsRepository.query(
       `select  id,word,translation,image,weight from (
-          SELECT  card.id as id,card.word as word,card.translation as translation,card.image as image,COALESCE(proficiency.weight, ${weight_inconnu}) as weight
-          FROM public.card
-          left join (select * from public.user_response where id_user=${id_user}) as A on card.id=A.id_card
-          left join public.proficiency on A.id_proficiency=proficiency.id_proficiency   where COALESCE(proficiency.weight, ${weight_inconnu}) in (${weight_inconnu},${weight_apprentissage})
-          union
+          SELECT  card.id as id,card.word as word,card.translation as translation,card.image as image,COALESCE(proficiency.weight, ${weight_inconnu}) as weight 
+          FROM public.card 
+          left join (select * from public.user_response where id_user=${id_user}) as A on card.id=A.id_card 
+          left join public.proficiency on A.id_proficiency=proficiency.id_proficiency   where COALESCE(proficiency.weight, ${weight_inconnu}) in (${weight_inconnu},${weight_apprentissage})   
+          union       
           SELECT  card.id as id,card.word as word ,card.translation as translation,card.image as image ,
-          proficiency.weight+TRUNC(DATE_PART('day', now()::timestamp - A.date_response::timestamp)/7)+answer.weight as weight FROM public.card
-          left join (select * from public.user_response where user_response.id_user=${id_user}) as A on card.id=A.id_card
-          left join public.proficiency on A.id_proficiency=proficiency.id_proficiency
-          left join public.answer on A.id_answer=answer.id
-          where proficiency.weight=${weight_aquis} )  as t1
+          proficiency.weight+TRUNC(DATE_PART('day', now()::timestamp - A.date_response::timestamp)/7)+answer.weight as weight FROM public.card    
+          left join (select * from public.user_response where user_response.id_user=${id_user}) as A on card.id=A.id_card    
+          left join public.proficiency on A.id_proficiency=proficiency.id_proficiency   
+          left join public.answer on A.id_answer=answer.id    
+          where proficiency.weight=${weight_aquis} )  as t1     
         ORDER BY (Random() * weight) desc limit ${nbrcards};`,
     );
     return card;
