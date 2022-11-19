ccfb350495a9385499aff9bd4913bc17c9c7a413
Abderrachid BELLAALI <91373823+Rachiid007@users.noreply.github.com>
2022-11-19 01:04:29 +0100

    🧪🚀 CI

---
+++ WEB/src/views/add-user/ChoiceHowToAddUser.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import MainHeader from '@/components/headers/MainHeader.vue'
+  import choiceHowToAddUser from '@/components/users/choiceHowToAddUser.vue'
+  import { useUserStore } from '@/stores/user'
+
+  const useStore = useUserStore()
+</script>
+
+<template>
+  <div class="page">
+    <MainHeader />
+    <choice-how-to-add-user />
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+  .page {
+    /* border : solid red 1px ;  */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 50px;
+    padding-top: 80px;
+  }
+</style>
