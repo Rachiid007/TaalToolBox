f178c066c332b4a1ca9f4bd53eef227fe018f5fc
Patson <cardin.tiako@gmail.com>
2022-11-25 23:27:48 +0100

    feat(school) : Find service in schoolclass

--- BACKEND/src/schoolclass/schoolclass.service.ts
+++ BACKEND/src/schoolclass/schoolclass.service.ts
@@ -1,11 +1,24 @@
+import { Schoolclass } from './entities/schoolclass.entity';
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateSchoolclassDto } from './dto/create-schoolclass.dto';
 import { UpdateSchoolclassDto } from './dto/update-schoolclass.dto';
 
 @Injectable()
 export class SchoolclassService {
-  create(createSchoolclassDto: CreateSchoolclassDto) {
-    return 'This action adds a new schoolclass';
+  constructor(
+    @InjectRepository(Schoolclass)
+    private schoolClassRepository: Repository<Schoolclass>,
+  ) {}
+  async create(schoolClass: { name: string; schoolId: number }) {
+    return await this.schoolClassRepository
+      .createQueryBuilder('schoolclass')
+      .insert()
+      .into('schoolclass', ['name', 'schoolId'])
+      .values(schoolClass)
+      .execute();
+    // return await this.schoolClassRepository.save(createSchoolclassDto);
   }
 
   findAll() {
@@ -14,6 +27,13 @@ export class SchoolclassService {
 
   findOne(id: number) {
     return `This action returns a #${id} schoolclass`;
+  }
+
+  async findClassUser(idSchool: number, classUser: string) {
+    return await this.schoolClassRepository
+      .createQueryBuilder('schoolclass')
+      .where({ name: classUser, school: idSchool })
+      .getOne();
   }
 
   update(id: number, updateSchoolclassDto: UpdateSchoolclassDto) {
