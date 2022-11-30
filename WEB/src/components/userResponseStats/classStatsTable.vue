1134d2f8a5064cb29d60798adce6aa340539803c
Ikram <HE201916@students.ephec.be>
2022-11-30 21:13:34 +0100

    [ADD] Suivi Avancement Apprentissage #66

--- WEB/src/components/userResponseStats/classStatsTable.vue
+++ WEB/src/components/userResponseStats/classStatsTable.vue
@@ -34,16 +34,16 @@
     { text: "JOUEUR", value: "joueur",width: 200 ,sortable: true },
     { text: "ECOLE", value: "ecole", sortable: true},
     { text: "CLASSE", value: "classe", sortable: true},
-    { text: "TOTALE MAITRISE", value: "maitrise", sortable: true},
-    { text: "TOTALE EN ACQUIS", value: "acquis"},
-    { text: "TOTALE EN APPRENTISSAGE", value: "apprentissage"},
+    { text: "TOTAL MAITRISE", value: "maitrise", sortable: true},
+    { text: "TOTAL EN ACQUIS", value: "acquis"},
+    { text: "TOTAL EN APPRENTISSAGE", value: "apprentissage"},
     { text: "DATE DERNIER JEUX", value: "lastPlay", width: 200, sortable: true},
   ];
   
   const items: Item[] = [
     { joueur: "Bernad C", ecole: "GSW", classe: "P1", maitrise: 25, acquis:3, lastPlay: "11/02/2022", apprentissage: 10},
     { joueur: "James A", ecole: "LAL", classe: "P1", maitrise: 10, acquis: 2, lastPlay: "12/02/2022", apprentissage: 21},
-    { joueur: "Kevin D", ecole: "BKN", classe: "P1", maitrise: 39, acquis: 1, lastPlay: "15/02/2022", apprentissage: 6},
+    { joueur: "Bernard D", ecole: "BKN", classe: "P1", maitrise: 39, acquis: 1, lastPlay: "15/02/2022", apprentissage: 6},
     { joueur: "Arthur T", ecole: "MIL", classe: "P2", maitrise: 5, acquis: 3, lastPlay: "17/02/2022", apprentissage: 2},
   ];
   </script>
@@ -67,13 +67,6 @@
     border-radius: 5px;
     width: 90%;
   }
-  .greetings {
-    margin-top: 40px;
-    margin-bottom: 30px;
-    font-size: 2.8em;
-    color: #026b30;
-    text-align: center;
-  }
   .info {
     display: flex;
     flex-direction: row;
@@ -92,129 +85,5 @@
     width: 1px;
     background-color: #707070;
     height: 400px;
-  }
-  .left_info {
-    width: 35%;
-    display: flex;
-    flex-direction: column;
-    gap: 40px;
-    align-self: flex-start;
-    margin-top: 35px;
-    text-align: center;
-  }
-  .imageLeft {
-    width: 100%;
-    height: auto;
-    align-self: center;
-  }
-  .right_info {
-    width: 35%;
-    display: flex;
-    flex-direction: column;
-    gap: 30px;
-    align-self: flex-start;
-    margin-top: 30px;
-    text-align: center;
-  }
-  .imageRight {
-    width: 55%;
-    height: auto;
-    align-self: center;
-  }
-  /* .image {
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-  } */
-  p {
-    font-size: 1.1em;
-  }
-  h1 {
-    font-size: 1.5em;
-  }
-  #firstIn {
-    opacity: 0;
-    transform: translateY(15%);
-    animation: fadeInBottomUp 1.5s ease 0.1s forwards;
-    animation-delay: 0.2s;
-  }
-  #secondIn {
-    opacity: 0;
-    transform: translateY(15%);
-    animation: fadeInBottomUp 1.5s ease 0.1s forwards;
-    animation-delay: 0.5s;
-  }
-  #thirdIn {
-    opacity: 0;
-    animation: fadeIn 1.5s ease 0.1s forwards;
-    animation-delay: 1.5s;
-  }
-  #fourthInLeft {
-    opacity: 0;
-    transform: translateX(-15%);
-    animation: fadeInLatteral 1.5s ease 0.1s forwards;
-    animation-delay: 1.5s;
-  }
-  #fourthInRight {
-    opacity: 0;
-    transform: translateX(15%);
-    animation: fadeInLatteral 1.5s ease 0.1s forwards;
-    animation-delay: 1.5s;
-  }
-  @media (max-width: 600px) {
-    .greetings {
-      font-size: 2em;
-    }
-    .info {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      gap: 50px;
-    }
-    .line {
-      height: 1px;
-      background-color: #707070;
-      width: 80vw;
-      display: none;
-    }
-    .left_info {
-      width: 80%;
-      display: flex;
-      flex-direction: column;
-      gap: 25px;
-      align-self: center;
-      margin-top: 35px;
-    }
-    .imageLeft {
-      height: 100%;
-      width: 100%;
-    }
-    .right_info {
-      width: 80%;
-      display: flex;
-      flex-direction: column;
-      gap: 30px;
-      align-self: center;
-    }
-    .imageRight {
-      height: 100%;
-      width: 70%;
-    }
-  }
-  @keyframes fadeInBottomUp {
-    100% {
-      opacity: 100%;
-      transform: translateY(0);
-    }
-  }
-  @keyframes fadeIn {
-    100% {
-      opacity: 100%;
-    }
-  }
-  @keyframes fadeInLatteral {
-    100% {
-      opacity: 100%;
-      transform: translateX(0);
-    }
   }
 </style>
--- WEB/src/components/userResponseStats/schoolStatsTable.vue
+++ WEB/src/components/userResponseStats/schoolStatsTable.vue
@@ -34,10 +34,10 @@
 
   const headers: Header[] = [
     { text: "ECOLE", value: "ecole", sortable: true},
-    { text: "TOTALE MAITRISE", value: "maitrise", sortable: true},
-    { text: "TOTALE EN ACQUIS", value: "acquis"},
-    { text: "TOTALE EN APPRENTISSAGE", value: "apprentissage"},
-    { text: "NOMBRE ETUDIANT JOUE", value: "nbrEtudiant", width: 200, sortable: true},
+    { text: "TOTAL MAITRISE", value: "maitrise", sortable: true},
+    { text: "TOTAL EN ACQUIS", value: "acquis"},
+    { text: "TOTAL EN APPRENTISSAGE", value: "apprentissage"},
+    { text: "NOMBRE DE PARTICIPANTS", value: "nbrEtudiant", width: 200, sortable: true},
   ];
   
   const items: Item[] = [
@@ -50,7 +50,7 @@
 
   
 <style scoped>
-  .main {
+.main {
     display: flex;
     flex-direction: column;
     max-width: 1600px;
@@ -66,13 +66,6 @@
     border: 1px solid #000;
     border-radius: 5px;
     width: 90%;
-  }
-  .greetings {
-    margin-top: 40px;
-    margin-bottom: 30px;
-    font-size: 2.8em;
-    color: #026b30;
-    text-align: center;
   }
   .info {
     display: flex;
@@ -92,129 +85,5 @@
     width: 1px;
     background-color: #707070;
     height: 400px;
-  }
-  .left_info {
-    width: 35%;
-    display: flex;
-    flex-direction: column;
-    gap: 40px;
-    align-self: flex-start;
-    margin-top: 35px;
-    text-align: center;
-  }
-  .imageLeft {
-    width: 100%;
-    height: auto;
-    align-self: center;
-  }
-  .right_info {
-    width: 35%;
-    display: flex;
-    flex-direction: column;
-    gap: 30px;
-    align-self: flex-start;
-    margin-top: 30px;
-    text-align: center;
-  }
-  .imageRight {
-    width: 55%;
-    height: auto;
-    align-self: center;
-  }
-  /* .image {
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-  } */
-  p {
-    font-size: 1.1em;
-  }
-  h1 {
-    font-size: 1.5em;
-  }
-  #firstIn {
-    opacity: 0;
-    transform: translateY(15%);
-    animation: fadeInBottomUp 1.5s ease 0.1s forwards;
-    animation-delay: 0.2s;
-  }
-  #secondIn {
-    opacity: 0;
-    transform: translateY(15%);
-    animation: fadeInBottomUp 1.5s ease 0.1s forwards;
-    animation-delay: 0.5s;
-  }
-  #thirdIn {
-    opacity: 0;
-    animation: fadeIn 1.5s ease 0.1s forwards;
-    animation-delay: 1.5s;
-  }
-  #fourthInLeft {
-    opacity: 0;
-    transform: translateX(-15%);
-    animation: fadeInLatteral 1.5s ease 0.1s forwards;
-    animation-delay: 1.5s;
-  }
-  #fourthInRight {
-    opacity: 0;
-    transform: translateX(15%);
-    animation: fadeInLatteral 1.5s ease 0.1s forwards;
-    animation-delay: 1.5s;
-  }
-  @media (max-width: 600px) {
-    .greetings {
-      font-size: 2em;
-    }
-    .info {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      gap: 50px;
-    }
-    .line {
-      height: 1px;
-      background-color: #707070;
-      width: 80vw;
-      display: none;
-    }
-    .left_info {
-      width: 80%;
-      display: flex;
-      flex-direction: column;
-      gap: 25px;
-      align-self: center;
-      margin-top: 35px;
-    }
-    .imageLeft {
-      height: 100%;
-      width: 100%;
-    }
-    .right_info {
-      width: 80%;
-      display: flex;
-      flex-direction: column;
-      gap: 30px;
-      align-self: center;
-    }
-    .imageRight {
-      height: 100%;
-      width: 70%;
-    }
-  }
-  @keyframes fadeInBottomUp {
-    100% {
-      opacity: 100%;
-      transform: translateY(0);
-    }
-  }
-  @keyframes fadeIn {
-    100% {
-      opacity: 100%;
-    }
-  }
-  @keyframes fadeInLatteral {
-    100% {
-      opacity: 100%;
-      transform: translateX(0);
-    }
   }
 </style>
