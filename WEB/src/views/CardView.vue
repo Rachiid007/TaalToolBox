596d436780fe717debe0dd25fd8056a1f3310e9d
Pourbaix Michaël <mi.pourbaix@students.ephec.be>
2022-12-07 18:14:01 +0100

    :sparkles: feat(css): Adding responsivity for some pages (Choose Activity, Flashcards)

--- WEB/src/views/CardView.vue
+++ WEB/src/views/CardView.vue
@@ -48,6 +48,7 @@
     margin: 0 auto;
     width: 100%;
     height: 100vh;
+    overflow: hidden;
   }
   .container {
     /* border: solid blue 1px; */
@@ -80,7 +81,8 @@
 
   @media (max-width: 768px) {
     .container {
-      padding: 10px;
+      padding: 0px;
+      flex-direction: column;
     }
     .rest-card {
       /* border: solid red 1px; */
@@ -88,7 +90,19 @@
       height: auto;
     }
     .play-card {
-      flex: 0.8;
+      justify-content: center;
+      flex: 0.6;
+    }
+    .card {
+      margin-left: 0;
+      padding: 5px;
+    }
+
+    .title {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      font-size: 10px;
     }
   }
   .title {
--- WEB/src/views/Login.vue
+++ WEB/src/views/Login.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
   import Login from '@/components/Login.vue'
   import MainHeader from '@/components/headers/MainHeader.vue'
-  import { useUserStore } from '@/stores/user';
+  import { useUserStore } from '@/stores/user'
 
   const userStore = useUserStore()
-
 </script>
 
 <template>
@@ -19,5 +18,6 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+    overflow-x: hidden;
   }
 </style>
