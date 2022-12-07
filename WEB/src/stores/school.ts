e93d3615b4870aeb4a438a46fa3f319ff57c8421
Aymar <71372488+HaAymar@users.noreply.github.com>
2022-12-07 20:15:24 +0100

    Add: (AjouterClasse) :Update Addclass

--- WEB/src/stores/school.ts
+++ WEB/src/stores/school.ts
@@ -2,18 +2,24 @@ import { defineStore } from 'pinia'
 import { ref, reactive, computed } from 'vue'
 import school from '@/services/school'
 
+interface objectClass {
+    id: number
+    name: string
+}
+
 export const useSchoolsStore = defineStore('school', () => {
-    const schoolList = ref<any[]>([])
+    const schoolList = ref<objectClass[]>([])
 
     const getSchoolName = async () => {
         const schoolRequest = await school.getSchool()
-
-
-        for (let i in schoolRequest.data){
-            schoolList.value.push(schoolRequest[i].name)
+        const schoolData = schoolRequest.data
+        console.log(schoolData)
+        for (let i in schoolData){
+            schoolList.value.push({id: schoolData[i].id, name: schoolData[i].name})    
         }
       }
-
-    return { schoolList }
+      console.log(schoolList.value)
+     
+    return { schoolList, getSchoolName }
 })
 
