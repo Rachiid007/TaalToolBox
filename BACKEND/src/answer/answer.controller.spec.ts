3b378e0dc692b2ae8b3f171a9a44f2e688bb6917
CardinPatson <cardin.tiako@gmail.com>
2022-12-11 16:11:13 +0100

    fix(answer): Fix answer repository When we run the backend the server crash due to wrong import [TEST] : implement unit test for this in spec

--- BACKEND/src/answer/answer.controller.spec.ts
+++ BACKEND/src/answer/answer.controller.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AnswerController } from './answer.controller';
 import { AnswerService } from './answer.service';
+import { Answer } from './entities/answer.entity';
+import { Repository } from 'typeorm';
+
+export class AnswerRepository extends Repository<Answer> {}
 
 import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
 
 const moduleMocker = new ModuleMocker(global);
 
 describe('AnswerController', () => {
+  // let answerController: AnswerController;
+  // let answerService: AnswerService;
   let controller: AnswerController;
 
   beforeEach(async () => {
@@ -50,33 +56,58 @@ describe('AnswerController', () => {
       .compile();
 
     controller = moduleRef.get<AnswerController>(AnswerController);
+    // Attention à bien mettre les dépendances que l'on retrouve dans AnswerService (constructeur)
+    // const module: TestingModule = await Test.createTestingModule({
+    //   providers: [
+    //     {
+    //       provide: 'AnswerRepository',
+    //       useClass: AnswerRepository,
+    //     },
+    //     AnswerService,
+    //   ],
+    //   controllers: [AnswerController],
+    // }).compile();
+    // answerService = module.get<AnswerService>(AnswerService);
+    // answerController = module.get<AnswerController>(AnswerController);
   });
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-
-  it('should return an array of answers', async () => {
-    const result = [
-      {
+  // answerService = new AnswerService(answerRepository);
+  // answerController = new AnswerController(answerService);
+  // Test unitaire sur la fonction answer
+  describe('findAll', () => {
+    it('should return an answer', async () => {
+      const result = {
+        description: 'Lutilisateur à trouvé la bonne réponse',
         id: 1,
         name: 'Jai trouvé',
-        description: 'Lutilisateur à trouvé la bonne réponse',
         weight: -1,
-      },
-      {
-        id: 2,
-        name: 'Presque trouvé',
-        description: 'Lélève à presque trouvé la bonne réponse',
-        weight: 0,
-      },
-      {
-        id: 3,
-        name: 'Je nai pas trouvé',
-        description: 'Lélève na pas trouvé la bonne réponse',
-        weight: 1,
-      },
-    ];
-    expect(await controller.findAll()).toEqual(result);
+      };
+      // Force la sortie à retourner la sortie que nous voulons (renvoie le résultat que nous voulons dans nos tests)
+      // jest.spyOn(answerService, 'findAll').mockImplementation(() => result);
+      expect(await controller.findAll()).toContainEqual(result);
+    });
+
+    it('should return an array of answers', async () => {
+      const result = [
+        {
+          id: 1,
+          name: 'Jai trouvé',
+          description: 'Lutilisateur à trouvé la bonne réponse',
+          weight: -1,
+        },
+        {
+          id: 2,
+          name: 'Presque trouvé',
+          description: 'Lélève à presque trouvé la bonne réponse',
+          weight: 0,
+        },
+        {
+          id: 3,
+          name: 'Je nai pas trouvé',
+          description: 'Lélève na pas trouvé la bonne réponse',
+          weight: 1,
+        },
+      ];
+      expect(await controller.findAll()).toEqual(result);
+    });
   });
 });
