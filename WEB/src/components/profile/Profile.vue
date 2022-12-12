dfa93ea617c01aa6dce0132b19b9e8f6e52941ac
Louis Lombaert <l.lombaert@students.ephec.be>
2022-12-12 17:14:41 +0100

    :sparkles: feat: get logged in user information

--- WEB/src/components/profile/Profile.vue
+++ WEB/src/components/profile/Profile.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="profile">
-        <div class="prenom">
-            Prenom
+        <div class="name">
+            {{user.name}} {{user.surname}}
         </div>
-        <div class="nom"> 
-            Nom
+        <div class="mail">
+            Adresse mail: {{user.email}}
+        </div>
+        <div class="birthdate">
+            Date de naissance: {{birthdate}}
+        </div>
+        <div class="phone">
+            Télephone: {{user.phone}}
         </div>
         <div class="photo">
             <img src="https://i.goopics.net/7rqy9u.jpg" width="200" height="200">
@@ -13,10 +19,18 @@
 </template>
 
 <script  setup lang="ts">
-
+    const user_json = localStorage.getItem('user');
+    const user = JSON.parse(user_json);
+    const birthdateStr = user.birthdate;
+    const birthdateTab = birthdateStr.split('-');
+    const birthdate = birthdateTab[2] + '/' + birthdateTab[1] + '/' + birthdateTab[0];
 </script>
 
 <style>
+    .name{
+        text-align: center;
+        font-size: 40px;
+    }
     .photo{
         display: block;
         float: right;
