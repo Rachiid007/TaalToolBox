29b07ac779bfe3583138c57971acd9bf7ae9e976
Patson <cardin.tiako@gmail.com>
2022-12-14 21:53:56 +0100

    test(ressource) test for level_difficulty, cards_theme, leve_map test(chooseActivitities) refactor choose test

--- BACKEND/src/cards_theme/cards_theme.controller.spec.ts
+++ BACKEND/src/cards_theme/cards_theme.controller.spec.ts
@@ -1,20 +1,43 @@
+import { CardsTheme } from './entities/cards_theme.entity';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CardsThemeController } from './cards_theme.controller';
 import { CardsThemeService } from './cards_theme.service';
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
 
+const moduleMocker = new ModuleMocker(global);
 describe('CardsThemeController', () => {
   let controller: CardsThemeController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CardsThemeController],
-      providers: [CardsThemeService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (token === CardsThemeService) {
+          return {
+            findAll: jest.fn().mockResolvedValue([
+              { id: 1, name: 'adjectif' },
+              { id: 2, name: 'alimentation' },
+            ]),
+          };
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     controller = module.get<CardsThemeController>(CardsThemeController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should contain theme', async () => {
+    expect(await controller.findAll()).toContainEqual({
+      id: 1,
+      name: 'adjectif',
+    });
   });
 });
--- BACKEND/src/level_map/level_map.controller.spec.ts
+++ BACKEND/src/level_map/level_map.controller.spec.ts
@@ -2,19 +2,56 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { LevelMapController } from './level_map.controller';
 import { LevelMapService } from './level_map.service';
 
+import { ModuleMocker, MockFunctionMetadata } from 'jest-mock';
+const moduleMocker = new ModuleMocker(global);
+
 describe('LevelMapController', () => {
   let controller: LevelMapController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LevelMapController],
-      providers: [LevelMapService],
-    }).compile();
+    })
+      .useMocker((token) => {
+        if (token === LevelMapService) {
+          return {
+            findAll: jest.fn().mockResolvedValue([
+              {
+                id: 6,
+                name: 'combo',
+                description: 'combo',
+                address: "Rue de l'Église Saint-Gilles, 1060, Saint-Gilles",
+                position: [4.3423450199618685, 50.83179500691212],
+                activityId: 3,
+                difficultyId: 1,
+                themeId: 7,
+              },
+            ]),
+          };
+        }
+        if (typeof token === 'function') {
+          const mockMetadata = moduleMocker.getMetadata(
+            token,
+          ) as MockFunctionMetadata<any, any>;
+          const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+          return new Mock();
+        }
+      })
+      .compile();
 
     controller = module.get<LevelMapController>(LevelMapController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should contain level map', async () => {
+    expect(await controller.findAll()).toContainEqual({
+      id: 6,
+      name: 'combo',
+      description: 'combo',
+      address: "Rue de l'Église Saint-Gilles, 1060, Saint-Gilles",
+      position: [4.3423450199618685, 50.83179500691212],
+      activityId: 3,
+      difficultyId: 1,
+      themeId: 7,
+    });
   });
 });
