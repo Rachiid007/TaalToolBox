1ef441f011aaee82c313a1deee54bb6fe77dac36
Louis Lombaert <l.lombaert@students.ephec.be>
2022-12-14 17:37:48 +0100

    :sparkles: feat: new tests

---
+++ WEB/src/components/__tests__/profile/profile.spec.ts
@@ -0,0 +1,28 @@
+import { shallowMount, mount } from '@vue/test-utils'
+import Profile from '@/components/profile/Profile.vue'
+import { beforeEach, describe, expect, test } from 'vitest'
+
+
+
+describe('Profile.vue', () => {
+    let wrapper = null
+
+    beforeEach(() => {
+        wrapper = mount(Profile)
+    })
+
+    test("number of divs", () => {
+        expect(wrapper.findAll("div").length).toEqual(6)
+    })
+
+    test("number of images", () => {
+        expect(wrapper.findAll("img").length).toEqual(4)
+    })
+
+    test("number of paragraphs", () => {
+        expect(wrapper.findAll("p").length).toEqual(3)
+    })
+    test("text of the name div", () => {
+        expect(wrapper.find(".name").text()).toEqual("Nom utilisateur")
+    })
+})
