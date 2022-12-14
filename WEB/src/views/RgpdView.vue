e38470a6e8c2f79c2baa6e1ff530b2dcd3ce1001
bricekouetcheu <brcronld@gmail.com>
2022-12-14 11:50:38 +0100

    creating gpdr page

--- WEB/src/views/RgpdView.vue
+++ WEB/src/views/RgpdView.vue
@@ -1,5 +1,96 @@
-<script></script>
+<script setup lang="ts">
+import FooterVue from '@/components/footer/Footer.vue';
 
-<template></template>
+</script>
 
-<style></style>
+<template>
+    <div class="main">
+        <div class="header">
+            <div class = "item1"> TaalToolBox: </div>
+            <div class="item2"> Mesures RGPD et traitement de données </div>
+        </div>
+
+        <div class="container">
+            <p> There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+            <p>
+                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+            </p>
+        </div>
+        
+    </div>
+    
+
+    <div class ="footer">
+        <FooterVue/>
+    </div>
+    
+
+    
+   
+
+</template>
+
+
+
+<style scoped>
+.main{
+    justify-items: center;
+    justify-content: center;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    min-height: 100%;
+}
+.header{
+    width: 100%;
+    max-width: 1500px;
+    border-bottom: 1px solid grey;
+    height: 90px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-family: NotoSans-Regular;
+    background-color: white;
+    margin: 0 auto;
+    top: 0;
+
+}
+
+.item1{
+    font-weight: bold;
+    color: #026b30;
+    font-size: 30px;
+}
+
+.item2{
+    font-weight: bold;
+    color: grey;
+    font-size: 20px;
+    
+}
+
+.container{
+    display: flex;
+    flex-direction: column;
+    font-size: 20px;
+    row-gap: 30px;
+    font-style: normal;
+    top: 100px;
+    color: black;
+    justify-content: center;
+    padding: 30px;
+   
+}
+
+.footer{
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    
+    
+}
+</style>
