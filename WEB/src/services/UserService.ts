c6b1c284e39ad6c467b1a3bfbef8b5dfeffc9a31
Abderrachid BELAALI <abderrachid.bellaali@gmail.com>
2022-12-15 15:26:05 +0100

    ✨ feat(add users excel): config component to use user store instead of service. #132 It's important before adding the tests to the component.

--- WEB/src/services/UserService.ts
+++ WEB/src/services/UserService.ts
@@ -1,30 +1,8 @@
 import { apiClient } from './apiClient'
-import type User from '@/types/user'
+import type { UserFromExcelFile } from '@/types/user'
 
 export default {
-  async getUsers(): Promise<User[]> {
-    const { data } = await apiClient.get('/users')
-    return data
-  },
-  async getUser(id: number): Promise<User> {
-    const { data } = await apiClient.get(`/users/${id}`)
-    return data
-  },
-  async createUser(user: User): Promise<User> {
-    const { data } = await apiClient.post('/users', user)
-    return data
-  },
-  // async updateUser(user: User): Promise<User> {
-  //   const { data } = await apiClient.put(`/users/${user.id}`, user)
-  //   return data
-  // },
-  async deleteUser(id: number): Promise<User> {
-    const { data } = await apiClient.delete(`/users/${id}`)
-    return data
-  },
-  // send a list of users to the server to be created
-  async createUsers(users: User[]): Promise<User[]> {
-    const { data } = await apiClient.post('/users/excel', users)
-    return data
+  async createUsers(users: UserFromExcelFile[]) {
+    return apiClient.post('/users/excel', users)
   }
 }
