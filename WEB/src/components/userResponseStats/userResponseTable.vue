5d3fc354d18321ca32c34fead478c6cba052282f
Ikram <HE201916@students.ephec.be>
2022-12-18 00:43:50 +0100

    [UPD] Add class and remove school from the tables #60

--- WEB/src/components/userResponseStats/userResponseTable.vue
+++ WEB/src/components/userResponseStats/userResponseTable.vue
@@ -32,7 +32,6 @@
 
   const headers: Header[] = [
     { text: "JOUEUR", value: "joueur",width: 200 ,sortable: true },
-    { text: "ECOLE", value: "ecole", sortable: true},
     { text: "CLASSE", value: "classe", sortable: true},
     { text: "TOTAL MAITRISE", value: "maitrise", sortable: true},
     { text: "TOTAL EN ACQUIS", value: "acquis"},
@@ -41,10 +40,10 @@
   ];
   
   const items: Item[] = [
-    { joueur: "Bernad C", ecole: "GSW", classe: "P1", maitrise: 25, acquis:3, lastPlay: "11/02/2022", apprentissage: 10},
-    { joueur: "James A", ecole: "LAL", classe: "P1", maitrise: 10, acquis: 2, lastPlay: "12/02/2022", apprentissage: 21},
-    { joueur: "Bernard D", ecole: "BKN", classe: "P1", maitrise: 39, acquis: 1, lastPlay: "15/02/2022", apprentissage: 6},
-    { joueur: "Arthur T", ecole: "MIL", classe: "P2", maitrise: 5, acquis: 3, lastPlay: "17/02/2022", apprentissage: 2},
+    { joueur: "Bernad C", classe: "1elec", maitrise: 25, acquis:3, lastPlay: "11/02/2022", apprentissage: 10},
+    { joueur: "James A", classe: "1elec", maitrise: 10, acquis: 2, lastPlay: "12/02/2022", apprentissage: 21},
+    { joueur: "Bernard D", classe: "1meca", maitrise: 39, acquis: 1, lastPlay: "15/02/2022", apprentissage: 6},
+    { joueur: "Arthur T", classe: "1IT", maitrise: 5, acquis: 3, lastPlay: "17/02/2022", apprentissage: 2},
   ];
   </script>
 
