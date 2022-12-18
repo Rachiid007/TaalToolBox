9211c563909747c54e44fcf4fbdd4ab32d0a3226
Ikram <HE201916@students.ephec.be>
2022-12-18 19:42:04 +0100

    [ADD] vue chart js and easy data table+ routes FE #66

--- WEB/src/router/index.ts
+++ WEB/src/router/index.ts
@@ -21,6 +21,7 @@ import AddClass from '@/views/Add Class/AddClass.vue'
 import InfoLevel from '@/views/Info level/InfoLevel.vue'
 import AddUserExcelView from '@/views/add-user/AddUserExcelView.vue'
 import Verbalize from '@/views/recognition/VerbalizeView.vue'
+import userResponseStats from '@/views/userResponseStats.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -129,8 +130,14 @@ const router = createRouter({
       path: '/verbalize',
       name: 'Verbalize',
       component: Verbalize
-    }
+    },
+    {
+      path: '/userResponseStats',
+      name: 'userResponseStats',
+      component: userResponseStats
+    }  
   ]
+  
 })
 
 export default router
